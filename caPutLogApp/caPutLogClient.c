f1eb5ed8cdfaf15a67de403fe8400dc96b374ccf
Dirk Zimoch <dirk.zimoch@psi.ch>
2021-09-16 10:13:19 +0200

    protect client list with mutex

--- caPutLogApp/caPutLogClient.c
+++ caPutLogApp/caPutLogClient.c
@@ -28,6 +28,8 @@
 #include <logClient.h>
 #include <epicsString.h>
 #include <epicsStdio.h>
+#include <epicsMutex.h>
+#include <cantProceed.h>
 
 #define epicsExportSharedSymbols
 #include "caPutLog.h"
@@ -43,21 +45,24 @@
 
 LOCAL READONLY ENV_PARAM EPICS_CA_PUT_LOG_ADDR = {"EPICS_CA_PUT_LOG_ADDR", ""};
 
-LOCAL struct clientlist {
+LOCAL struct clientItem {
     logClientId caPutLogClient;
-    struct clientlist *next;
+    struct clientItem *next;
     char addr[1];
 } *caPutLogClients = NULL;
+static epicsMutexId caPutLogClientsMutex;
 
 /*
  *  caPutLogClientFlush ()
  */
 void caPutLogClientFlush ()
 {
-    struct clientlist* c;
+    struct clientItem* c;
+    epicsMutexMustLock(caPutLogClientsMutex);
     for (c = caPutLogClients; c; c = c->next) {
         logClientFlush (c->caPutLogClient);
     }
+    epicsMutexUnlock(caPutLogClientsMutex);
 }
 
 /*
@@ -65,10 +70,12 @@ void caPutLogClientFlush ()
  */
 void caPutLogClientShow (unsigned level)
 {
-    struct clientlist* c;
+    struct clientItem* c;
+    epicsMutexMustLock(caPutLogClientsMutex);
     for (c = caPutLogClients; c; c = c->next) {
         logClientShow (c->caPutLogClient, level);
     }
+    epicsMutexUnlock(caPutLogClientsMutex);
 }
 
 /*
@@ -79,12 +86,14 @@ int caPutLogClientInit (const char *addr_str)
     int status;
     struct sockaddr_in saddr;
     unsigned short default_port = 7011;
-    struct clientlist** pclient;
+    struct clientItem** pclient;
     char *clientaddr;
     char *saveptr;
     char *addr_str_copy1;
     char *addr_str_copy2;
 
+    if (!caPutLogClientsMutex)
+        caPutLogClientsMutex = epicsMutexMustCreate();
     if (!addr_str || !addr_str[0]) {
         if (caPutLogClients) return caPutLogSuccess;
         addr_str = envGetConfigParamPtr(&EPICS_CA_PUT_LOG_ADDR);
@@ -96,6 +105,7 @@ int caPutLogClientInit (const char *addr_str)
 
     addr_str_copy2 = addr_str_copy1 = epicsStrDup(addr_str);
 
+    epicsMutexMustLock(caPutLogClientsMutex);
     while(1) {
         clientaddr = strtok_r(addr_str_copy1, " \t\n\r", &saveptr);
         if (!clientaddr) break;
@@ -115,11 +125,7 @@ int caPutLogClientInit (const char *addr_str)
             continue;
         }
 
-        *pclient = malloc(sizeof(struct clientlist)+strlen(clientaddr));
-        if (!*pclient) {
-            fprintf (stderr, "caPutLog: out of memory\n");
-            return caPutLogError;
-        }
+        *pclient = callocMustSucceed(1,sizeof(struct clientItem)+strlen(clientaddr),"caPutLog");
         strcpy((*pclient)->addr, clientaddr);
 
         (*pclient)->caPutLogClient = logClientCreate (saddr.sin_addr, ntohs(saddr.sin_port));
@@ -132,6 +138,7 @@ int caPutLogClientInit (const char *addr_str)
 
         (*pclient)->next = NULL;
     }
+    epicsMutexUnlock(caPutLogClientsMutex);
     free(addr_str_copy2);
     return caPutLogClients ? caPutLogSuccess : caPutLogError;
 }
@@ -141,8 +148,10 @@ int caPutLogClientInit (const char *addr_str)
  */
 void caPutLogClientSend (const char *message)
 {
-    struct clientlist* c;
+    struct clientItem* c;
+    epicsMutexMustLock(caPutLogClientsMutex);
     for (c = caPutLogClients; c; c = c->next) {
         logClientSend (c->caPutLogClient, message);
     }
+    epicsMutexUnlock(caPutLogClientsMutex);
 }
